5b8116206b780d20e59e5b36258829136f5744af
Marjo Perttu <marjo.perttu@gmail.com>
2025-03-24 12:43:00 +0200

    Muokattu yhteysasetuksia

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 if (!isset($db)) {
-    $y_tiedot = "dbname=hcmape user=hcmape password=x8IQXiqOettDpXp";
+    $y_tiedot = "dbname=hcmape user=hcmape password=salasana_tulee_syottaa";
     $db = pg_connect($y_tiedot);
     if (!$db) {
         die("Tietokantayhteyden luominen epäonnistui: " . pg_last_error());
