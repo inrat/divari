de9617da7448099019b1c2477117167aeec53932
Inka <inratmail@gmail.com>
2025-03-26 15:56:54 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 if (!isset($db)) {
-    $y_tiedot = "dbname=dcinra user=dcinra password=4zVb9ZVXDIxXudP";
+    $y_tiedot = "dbname=hcmape user=hcmape password=salasana_tulee_syottaa";
     $db = pg_connect($y_tiedot);
     if (!$db) {
         die("Tietokantayhteyden luominen epäonnistui: " . pg_last_error());
