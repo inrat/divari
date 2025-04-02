1ebce0ef0df307d47da1f128d0775c8cc6ea54be
Inka <inka.ratia@tuni.fi>
2025-04-02 07:27:08 +0300

    Lisätään admin-kirjautumisen mahdollistavat tiedostot.

--- public/admin.php
+++ public/admin.php
@@ -1,1 +1,16 @@
-// admin-hallinta esim. synkronointi
+<?p<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Sivu - <?php echo isset($_SESSION['nimi']) ? $_SESSION['nimi'] : 'Not Logged In'; ?></title>
+</head>
+<body>
+    <?php if (isset($_SESSION['nimi'])): ?>
+        <h1>Hei, <?php echo $_SESSION['nimi']; ?>!</h1>
+    <?php else: ?>
+        <h1>Hei!</h1>
+    <?php endif; ?>
+</body>
+</html>
