5ee8c1e4a5ab66935a0419294faad99df86a2684
Inka <inka.ratia@tuni.fi>
2025-04-16 16:01:40 +0300

    Lisätään virheviestejä vääräntyyppisen datan syötöstä.

--- public/admin.php
+++ public/admin.php
@@ -8,9 +8,12 @@
 <body>
     <?php if (isset($_SESSION['nimi'])): ?>
         <h1>Hei, <?php echo $_SESSION['nimi']; ?>!</h1>
-        <a href="teoslisays.php" class="button">Lis&auml;&auml; ja tarkastele teoksia</a>
+        <a href="teos_ja_nide_lisays.php" class="button">Lis&auml;&auml; ja tarkastele teoksia</a>
     <?php else: ?>
         <h1>Hei!</h1>
+        <p>Et ole kirjautuneena Adminiin. Siirry kirjautumiseen 
+            <a href="admin_login_popup.php"> t&auml;st&auml;.</a>
+        </p>
     <?php endif; ?>
 </body>
 </html>
