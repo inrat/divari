d4455f54e8c7b5639ca07893c0784afd1962dc20
Inka <inka.ratia@tuni.fi>
2025-04-21 21:13:45 +0300

    Lisätään tyylit adminiin.

--- public/admin_login_popup.php
+++ public/admin_login_popup.php
@@ -6,26 +6,31 @@ require_once __DIR__ . '/../config/config.php';
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
     <title>Keskusdivari - Admin-kirjautuminen</title>
+    <link rel="stylesheet" href="style3.css">
+</head>
 </head>
 <body>
-    <h2>Kirjaudu sis&auml;&auml;n</h2>
+    <div class="container">    
+        <h2>Kirjaudu sis&auml;&auml;n</h2>
 
-    <?php
-    if (isset($_SESSION['message'])) { 
-        echo "<p>" . $_SESSION['message'] . "</p>"; 
-        unset($_SESSION['message']); 
-    } 
-    ?>
+        <?php
+        if (isset($_SESSION['message'])) { 
+            echo "<p>" . $_SESSION['message'] . "</p>"; 
+            unset($_SESSION['message']); 
+        } 
+        ?>
 
-    <form action="admin_login.php" method="POST">
-        <label for="divari_id">Divarin ID:</label>
-        <input type="text" name="divari_id" required>
-        <br>
-        <label for="password">Salasana:</label>
-        <input type="password" name="password" required>
-        <br>
-        <button type="submit">Kirjaudu</button>
-    </form>
+        <form action="admin_login.php" method="POST">
+            <label for="divari_id">Divarin ID:</label>
+            <input type="text" name="divari_id" required>
+            <br>
+            <label for="password">Salasana:</label>
+            <input type="password" name="password" required>
+            <br>
+            <button type="submit">Kirjaudu</button>
+        </form>
+    </div>
 </body>
 </html>
