460462009d721ae48dd3175ceaf349b00948974c
Inka <inka.ratia@tuni.fi>
2025-04-21 15:46:45 +0300

    Lisätään painike uloskirjautumiselle.

--- public/admin.php
+++ public/admin.php
@@ -4,16 +4,29 @@
 <html>
 <head>
     <title>Admin Sivu - <?php echo isset($_SESSION['nimi']) ? $_SESSION['nimi'] : 'Not Logged In'; ?></title>
+    <link rel="stylesheet" href="style3.css">
 </head>
 <body>
+    <div class="container">
     <?php if (isset($_SESSION['nimi'])): ?>
-        <h1>Hei, <?php echo $_SESSION['nimi']; ?>!</h1>
-        <a href="teos_ja_nide_lisays.php" class="button">Lis&auml;&auml; ja tarkastele teoksia</a>
+        <a href="logout.php" class="logout-link">Kirjaudu ulos</a>
+        <h1>Hei, <?php echo $_SESSION['nimi']; $_SESSION['divari_id'] ?>!</h1>
+        <a href="teos_ja_nide_lisays.php" class="button">Lis&auml;&auml; ja tarkastele omia niteit&auml;</a>
+        <br>
+        <?php if (isset($_SESSION['divari_id']) && $_SESSION['divari_id'] == 1): ?>
+            <a href="admin_raport.php" class="button">Tarkastele kaikkia myynniss&auml; olevia teoksia</a>
+            <br>
+            <a href="asiakkaat.php" class="button">Tarkastele asiakkaita</a>
+            <br>
+        <?php endif; ?>
+        
     <?php else: ?>
         <h1>Hei!</h1>
         <p>Et ole kirjautuneena Adminiin. Siirry kirjautumiseen 
             <a href="admin_login_popup.php"> t&auml;st&auml;.</a>
         </p>
+
     <?php endif; ?>
+    </div>
 </body>
 </html>
