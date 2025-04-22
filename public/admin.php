46b3754b95bf2df70154e835fadc24f50e0362c5
Inka <inratmail@gmail.com>
2025-04-22 12:56:44 +0300

    Päivitä admin.php ottamaan huomioon osatietokannat

--- public/admin.php
+++ public/admin.php
@@ -1,5 +1,9 @@
 <!-- admin.php -->
-<?php session_start(); ?>
+<?php
+session_start();
+require_once __DIR__ . '/../config/config.php';
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,10 +12,10 @@
 </head>
 <body>
     <div class="container">
-    <?php if (isset($_SESSION['nimi'])): ?>
-
+    <?php if (isset($_SESSION['nimi'])) { ?>
+        <?php $divari_id =  $_SESSION['divari_id']; ?>
         <a href="logout.php" class="logout-link">Kirjaudu ulos</a>
-        <h1>Hei, <?php echo $_SESSION['nimi']; $_SESSION['divari_id'] ?>!</h1>
+        <h1>Hei, <?php echo $_SESSION['nimi']; ?>!</h1>
         <a href="teos_ja_nide_lisays.php" class="button">Lis&auml;&auml; ja tarkastele omia niteit&auml;</a>
         <br>
         <?php if (isset($_SESSION['divari_id']) && $_SESSION['divari_id'] == 1): ?>
@@ -20,14 +24,27 @@
             <a href="asiakkaat.php" class="button">Tarkastele asiakkaita</a>
             <br>
         <?php endif; ?>
-        
-    <?php else: ?>
+
+        <!-- Tarkistetaan onko kirjautuneella adminilla korreloiva osatietokanta. Näytetään vaihteohtona synkrointi
+         vain, jos sellainen löytyy. -->
+        <?php  $schema_name = 'divari_' . $divari_id; ?>
+
+        <?php $query = "SELECT schema_name
+        FROM information_schema.schemata
+        WHERE schema_name = $1"; ?>
+
+        <?php $result = pg_query_params($db, $query, array($schema_name)); ?>
+
+        <?php if ($result && pg_num_rows($result) > 0) { ?>
+             <a href="admin_synkronointi.php" class="button">Tarkista keskus- ja oman tietokannan synkronointi </a>
+        <?php } ?>
+      
+<?php } else { ?>
         <h1>Hei!</h1>
         <p>Et ole kirjautuneena Adminiin. Siirry kirjautumiseen 
             <a href="admin_login_popup.php"> t&auml;st&auml;.</a>
         </p>
-
-    <?php endif; ?>
+    <?php } ?>
     </div>
 </body>
 </html>
